3e9e22117cd9472f004cc484d812a1f0dfe66559
Sukjoon Kim <sjoonk@gmail.com>
2019-09-11 17:07:36 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Custom Snippets
+==
+WordPress site-specific plugin base
+
+Usage:
+
+1. Download the zip file.
+2. Upload the zip file to WordPress plugin folder.
+3. Edit & Update the plugin as you wish.
+
+
+
+
---
+++ custom-snippets.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Custom Snippets
+Description: Site specific code changes for this site
+Version: 0.1
+Author: Useful Paradigm
+Author URI: https://www.usefulparadigm.com
+*/
+ 
+//Now start placing your customization code below this line
